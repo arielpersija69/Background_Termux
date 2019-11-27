ff59bf559df048bc98b95c5840dee538fc2a000a
Ariel Sandy Permana <56505222+arielpersija69@users.noreply.github.com>
2019-11-27 19:55:14 +0700

    Add files via upload

---
+++ hah.sh
@@ -0,0 +1,115 @@
+clear
+cd /sdcard/Download/Apriyani1
+sh Apriyani.sh
+cd
+clear
+black='\e[0;30m'
+blue='\e[0;34m'
+green='\e[0;32m'
+cyan='\e[0;36m'
+red='\e[0;31m'
+purple='\e[0;35m'
+brown='\e[0;33m'
+lightgray='\e[0;37m'
+darkgray='\e[1;30m'
+lightblue='\e[1;34m'
+lightgreen='\e[1;32m'
+lightcyan='\e[1;36m'
+lightred='\e[1;31m'
+lightpurple='\e[1;35m'
+yellow='\e[1;33m'
+white='\e[1;37m'
+nc='\e[0m'
+echo
+#Figlet nama
+echo
+echo
+echo -e '\e[1;36m''                       fLWWW####WEDG                        '
+echo -e '\e[1;36m''                 DE##################W#W#WE                 '
+echo -e '\e[1;36m''             L#################################t            '
+echo -e '\e[1;36m''          K######################################           '
+echo -e '\e[1;36m''         #########################################          '
+echo -e '\e[1;36m''        E##########################################         '
+echo -e '\e[1;36m''        ###########################################W        '
+echo -e '\e[1;36m''       ;############################################        '
+echo -e '\e[1;36m''       K############################################W       '
+echo -e '\e[1;36m''       W#######KW##########################WW########       '
+echo -e '\e[1;36m''       #####fLLLLLLf###################WLLLLLLL######.      '
+echo -e '\e[1;36m''      .W###LLf  i#GLLL###############ELLLKE  ,LLL#####      '
+echo -e '\e[1;36m''      E###L         W##W############GW#L        L#####      '
+echo -e '\e[1;36m''      ###E  fLLi      ###############       fLL; L####      '
+echo -e '\e[1;36m''      ##W LLLLLLLLL     ############    ;LLLLLLLLLE###      '
+echo -e '\e[1;36m''      ###########LLLL    ##########   ;LLL############;     '
+echo -e '\e[1;36m''      #############GLLL   ########   LLL##############W     '
+echo -e '\e[1;36m''      ###############LLLi ######## LLLL################     '
+echo -e '\e[1;36m''      ################LLf##########LLW#################     '
+echo -e '\e[1;36m''      #################LL#########ELW##################     '
+echo -e '\e[1;36m''     :########:,,i,,,W##L#########L####;,,,,,##########     '
+echo -e '\e[1;36m''     ,#K#####,##W   KD,##f########L##,,#KE#W#,t########     '
+echo -e '\e[1;36m''     i##W##W,#         ,j#######EW#;,:       #,E####W##     '
+echo -e '\e[1;36m''     L##LW#,,           ,#######G##,           ,D##W###     '
+echo -e '\e[1;36m''     K###,,,,WK       t,;#######G##,D         #,,,j####     '
+echo -e '\e[1;36m''     G#fKL##K,,,,it;,,,####W####D###:,;W###K;,,t#f,W###     '
+echo -e '\e[1;36m''     L#t#######D,,,j######EW####D####WK,,,,,,W#W###W#W#     '
+echo -e '\e[1;36m''     ,E###################DK####DD#####################     '
+echo -e '\e[1;36m''     :####################DW####DD####################W     '
+echo -e '\e[1;36m''     :###################DDW####DD#####################     '
+echo -e '\e[1;36m''      ###################DDW####DDD####################     '
+echo -e '\e[1;36m''      ##################DDD#####DDDE##################E     '
+echo -e '\e[1;36m''      #################DD#D#####DD#D##################:     '
+echo -e '\e[1;36m''      ##############W##D##D#####KD##D#################      '
+echo -e '\e[1;36m''      ##############f#D###D######D###G#fW##########W##      '
+echo -e '\e[1;36m''      ##f#########ffG#####D######D###D#fff#########L##      '
+echo -e '\e[1;36m''      ##fLf###ELLLK###W###D######D###D###GffL####LLL##      '
+echo -e '\e[1;36m''      G##LL   ########## #DD####DK#W#########K;;jfL###      '
+echo -e '\e[1;36m''      .##LL#  ###########f#WDDDD#j ##########t  #LL##L      '
+echo -e '\e[1;36m''       ##KLDL  ########### L#### ############  WDLE##       '
+echo -e '\e[1;36m''       ###Lf#   #########,        ##########  ;WLL###       '
+echo -e '\e[1;36m''       K##WLDK  W#######K    E     #######W   #ELW###       '
+echo -e '\e[1;36m''       :###fL#   GWWWKt     .##       iDKK   ##LL###L       '
+echo -e '\e[1;36m''        ####LGW             ####            ##LL####        '
+echo -e '\e[1;36m''        ####fLW#           W#####          j#LLf####        '
+echo -e '\e[1;36m''        ,####LLG#####WWW            #WWW####LfL####E        '
+echo -e '\e[1;36m''         #####L#LW##W###WW########W#WW#####f#L#####         '
+echo -e '\e[1;36m''         L####DL#W###W#####WWWWWW########K##fG####W         '
+echo -e '\e[1;36m''          #####f####DW####W############L####L#####          '
+echo -e '\e[1;36m''          .#####L###############W##########L#####L          '
+echo -e '\e[1;36m''           E#####f########W     ##########f#####W           '
+echo -e '\e[1;36m''            K###############    ################            '
+echo -e '\e[1;36m''             W####W#########   ##########K#####             '
+echo -e '\e[1;36m''              W####E########   j########D#####:             '
+echo -e '\e[1;36m''               E############    #############               '
+echo -e '\e[1;36m''                ###########W    ############                '
+echo -e '\e[1;36m''                 E##########    ##########W                 '
+echo -e '\e[1;36m''                  W#########    ##########                  '
+echo -e '\e[1;36m''                   L#######W    ########L                   '
+echo -e '\e[1;36m''                    G#######    ######WL                    '
+echo -e '\e[1;36m''                     f######   f######                      '
+echo -e '\e[1;36m''                       #####.  #####W                       '
+echo -e '\e[1;36m''                        G####  ####t                        '
+echo -e '\e[1;36m''                          K##  ##D                          '
+echo -e '\e[1;36m''                             j                              '
+echo
+figlet -f emboss ANONYMOUS INDONESIA | lolcat
+echo
+echo -ne 'Today is:\t\t\e[1;31m' Wed Nov 27 17:46:02 WIB 2019; echo ''
+echo -e 'Kernel Information: \t\e[1;31m' Linux 4.4.83 armv7l
+echo
+echo -e '\e[0;36m' ' Nama TEAM = Indonesian Cyber Team'
+echo
+echo "AUTHOR = Ariel Sandy Permana
+=================
+YouTube : Ariel 69 Channel
+=================
+Github : arielpersija69
+================="
+echo
+echo -e '\e[0;36m' ''the quieter you become, the more you able to hear''
+echo
+echo -e '\e[0;35m' '<|-----------------------------------------------------------------|>'
+echo
+command_not_found_handle() {
+        /data/data/com.termux/files/usr/libexec/termux/command-not-found 
+}
+
+PS1='\033[01;34m\]┌──\[\033[01;32m\]Ariel Kun\[\033[01;34m\]@ERROR404\[\033[01;31m\]\[\033[00;34m\]\[\033[01;34m\]\w\[\033[00;34m\]\[\033[01;32m\]: \[\033[01;34m\]└╼\[\033[01;31m\]#\[\033[01;32m\]'
